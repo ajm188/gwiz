4b655858b75dcb1f890105d188e2a42245b8f14c
Andrew Mason <andrew@fixedpoint.xyz>
2016-11-13 17:06:38 -0800

    Adds our nifty handler-wrapper and a simple query!!

--- handlers/zombies.go
+++ handlers/zombies.go
@@ -5,20 +5,24 @@ import (
 	"net/http"
 )
 
+import (
+	"github.com/ajm188/gwiz/db"
+)
+
 func zombies() *http.ServeMux {
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/detail/", zombieDetail)
 	mux.HandleFunc("/new/", zombieNew)
-	mux.HandleFunc("/", zombieBase)
+	mux.HandleFunc("/", WithConnectionFunc(zombieBase))
 
 	return mux
 }
 
-func zombieBase(w http.ResponseWriter, r *http.Request) {
+func zombieBase(conn db.Connection, w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case "GET":
-		zombieIndex(w, r)
+		zombieIndex(conn, w, r)
 	case "POST":
 		zombieCreate(w, r)
 	default:
@@ -34,8 +38,17 @@ func zombieNew(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "./templates/zombies/new.html")
 }
 
-func zombieIndex(w http.ResponseWriter, r *http.Request) {
+func zombieIndex(conn db.Connection, w http.ResponseWriter, r *http.Request) {
+	var count int
+
 	fmt.Fprintf(w, "Listing zombies\n")
+	err := conn.QueryRow("SELECT COUNT(*) FROM zombies").Scan(&count)
+	if err != nil {
+		fmt.Fprintf(w, "Got %s while performing query\n", err)
+		return
+	}
+
+	fmt.Fprintf(w, "%d\n", count)
 }
 
 func zombieCreate(w http.ResponseWriter, r *http.Request) {
