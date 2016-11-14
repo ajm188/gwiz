d19f9a788925a52b0b181767be3b248994df3774
Andrew Mason <andrew@fixedpoint.xyz>
2016-11-13 18:29:26 -0800

    Adds successful INSERT query

--- handlers/zombies.go
+++ handlers/zombies.go
@@ -24,7 +24,7 @@ func zombieBase(conn db.Connection, w http.ResponseWriter, r *http.Request) {
 	case "GET":
 		zombieIndex(conn, w, r)
 	case "POST":
-		zombieCreate(w, r)
+		zombieCreate(conn, w, r)
 	default:
 		fmt.Fprintf(w, "%s: %s\n", r.Method, r.RequestURI)
 	}
@@ -51,12 +51,35 @@ func zombieIndex(conn db.Connection, w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "%d\n", count)
 }
 
-func zombieCreate(w http.ResponseWriter, r *http.Request) {
+func zombieCreate(conn db.Connection, w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "I see you are trying to create a zombie!\n")
 	if err := r.ParseForm(); err != nil {
 		fmt.Fprintf(w, "Uh oh. Got %s when parsing form", err)
 		return
 	}
 	name := r.FormValue("name")
-	fmt.Fprintf(w, "You asked to create a zombie with name: %s", name)
+	fmt.Fprintf(w, "You asked to create a zombie with name: %s\n", name)
+
+	stmt, err := conn.Prepare("INSERT INTO zombies (name) VALUES ($1) RETURNING id;")
+	if err != nil {
+		fmt.Fprintf(w, "%s", err)
+		return
+	}
+
+	rows, err := stmt.Query(name)
+	if err != nil {
+		fmt.Fprintf(w, "%s", err)
+		return
+	}
+
+	defer rows.Close()
+	for rows.Next() {
+		var id int
+		if err := rows.Scan(&id); err != nil {
+			fmt.Fprintf(w, "%s", err)
+			return
+		}
+		fmt.Fprintf(w, "That zombie was created with id %d\n", id)
+	}
+	fmt.Fprintf(w, "One day this will 302 you to that zombie's page\n")
 }
