4b1081ea9440d762bc1a0fe42d56e7053d84b327
Andrew Mason <andrew@fixedpoint.xyz>
2016-11-14 19:09:04 -0800

    Adds two more wrappers for our new request type

--- handlers/wrappers.go
+++ handlers/wrappers.go
@@ -8,6 +8,37 @@ import (
 	"github.com/ajm188/gwiz/db"
 )
 
+type RequestFunc func(*Request)
+
+func WithRequest(f RequestFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		req := Request{
+			w,
+			r,
+			nil,
+		}
+		f(&req)
+	}
+}
+
+func WithConnection(f RequestFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		req := Request{
+			w,
+			r,
+			nil,
+		}
+		conn, err := db.NewConnection(nil)
+		if err != nil {
+			req.Error(500, err)
+			return
+		}
+		defer conn.Close()
+		req.Connection = conn
+		f(&req)
+	}
+}
+
 type ConnectionHandlerFunc func(db.Connection, http.ResponseWriter, *http.Request)
 
 func WithConnectionFunc(handlerFunc ConnectionHandlerFunc) http.HandlerFunc {
